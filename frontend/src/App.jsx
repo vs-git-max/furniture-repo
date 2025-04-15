33876f604076689cbf420b9312b4a34d98f49ee0
Sam Chris <samlearningcode14@gmail.com>
2025-04-15 05:24:41 +0300

    added some assets then cretaed some components and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-center px-8 py-1 bg-[#fcefef] mt-2 w-[90%] text-center">
+      <div className="text-xl font-bold text-[#012538] logoClass">
+        sam furniture
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
